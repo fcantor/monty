30249fca293bdbb15c9e4370ee8060fe7233c371
aslebloas <aslebloas@gmail.com>
2018-09-26 23:18:04 +0000

    Implement pall function

---
+++ pall.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * pall - print all
+ * @stack: ponter to the list stack
+ * @line_number: unused variable
+ * Return: the address of the first node
+ */
+stack_t *pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
+{
+	stack_t *tmp;
+	size_t node_c = 0;
+
+	tmp = *stack;
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		node_c++;
+	}
+
+	return (*stack);
+}
