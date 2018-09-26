41fb4e2a140ee44a178462d71351d9e4daf442ee
Francesca Cantor <fpcantor@gmail.com>
2018-09-26 21:15:33 +0000

    Added a copy-paste version of main and op_func from test branch

---
+++ main.c
@@ -0,0 +1,88 @@
+nclude "monty.h"
+
+/* Initialize the global variable */
+int token = 0;
+
+/**
+ * main - Interprets bytecode
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	const char *filename;
+	char *string, *opcode;
+	size_t nbytes = 1;
+	FILE *file;
+	unsigned int line_num = 0;
+	ssize_t read_c = 0;
+	stack_t *stack, *node;
+	char *error;
+
+	stack = NULL;
+	if (argc == 2)
+	{
+		filename = argv[1];
+
+		/* Open File with the bytecodes */
+		file = fopen(filename, "r");
+		if (file == NULL)
+		{
+			printf("Failed to open file\n");
+			exit(1);
+		}
+
+		/* Read file line by line */
+		while (read_c != EOF)
+		{
+			string = NULL;
+			read_c = getline(&string, &nbytes, file);
+			if (read_c == -1)
+			{
+				free(string);
+				fclose(file);
+				exit(1);
+			}
+
+			/* Keep count of the number of lines */
+			line_num++;
+
+			/* Continue if line had only the null byte */
+			if (read_c == 0)
+				continue;
+
+			/* Continue if line had only the new line character */
+			if (read_c == 1)
+			{
+				free(string);
+				continue;
+			}
+
+			/* Parse the first elements of the line */
+			opcode = strtok(string, " ");
+
+			token = atoi(strtok(NULL, " "));
+			printf("token: %d\n", token);
+			printf("%s", opcode);
+
+			node = op_func(opcode)(&stack, line_num);
+
+			printf("After push:\n");
+			printf("%s %d == %d\n", opcode, node->n, token);
+		}
+		/* Free memory and close the file */
+
+		free_stack(stack);
+
+		fclose(file);
+	}
+	else
+	{
+		error = "Wrong number of arguments\n";
+		write(STDERR_FILENO, error, strlen(error));
+		exit(99);
+	}
+
+	return (0);
+}
