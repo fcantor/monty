83a081a4d6b16fc85a75ccaedcc5bdefb013ca29
Francesca Cantor <fpcantor@gmail.com>
2018-09-27 23:41:30 +0000

    Fixed betty styling error

--- add.c
+++ add.c
@@ -17,7 +17,8 @@ stack_t *add(stack_t **stack, unsigned int line_number)
 	if (!head || !head->next)
 	{
 		/* Print error, free, and exit */
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n",
+line_number);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
