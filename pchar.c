b06926ac1962e1efd4aa0859b12264cca3692ebe
aslebloas <aslebloas@gmail.com>
2018-09-28 00:30:37 +0000

    Print error if pchar is called on a non ascii char

--- pchar.c
+++ pchar.c
@@ -3,11 +3,11 @@
 /**
  * pchar - prints the char at the top of the stack
  * @stack: The stack list
- * @line_number: The line number of the file
+ * @line_num: The line number of the file
  * Return: The head of the stack
  */
 
-stack_t *pchar(stack_t **stack, unsigned int line_number)
+stack_t *pchar(stack_t **stack, unsigned int line_num)
 {
 	stack_t *head = *stack;
 
@@ -15,7 +15,14 @@ stack_t *pchar(stack_t **stack, unsigned int line_number)
 	if (!head)
 	{
 		/* Print error, free, and exit */
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+	if (head->n < 0 || head->n > 127)
+	{
+		fprintf(stderr,
+			"L%d: can't pchar, value out of range\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 
